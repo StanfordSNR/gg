70aa2d1e58e2501c1b3b83cb6c248d169d4ed280
Dan Iter <daniter@stanford.edu>
2017-04-13 14:46:05 -0700

    added gg-gcc to build and switched to using vectors instead of char**

--- src/frontend/gg-gcc.cc
+++ src/frontend/gg-gcc.cc
@@ -9,25 +9,27 @@
 #include <ctime>
 #include <sys/time.h>
 #include <chrono>
+#include <vector>
 
+using namespace std;
 
 const char *PREPROC_FLAG = "-E";
 const char *COMPILER = "gcc";
 const char *OUTFLAG = "-o";
 const char *COMPILEFLAG = "-c";
 const char PREPROC_SUFFIX = 'i';
-bool verbose = false;
+bool verbose = false; // TODO add to commandline args
 const char *COMPILE_COMMAND[] = {"gcc", "-g", "-O2", "-c", "-o", "",  "", '\0'};
 const int OUTINDEX = 5;
 const int SRCINDEX = OUTINDEX+1;
 
 
 
-bool getPreprocArgs(int argc, char *argv[], char*preproc_args[], char object_file[]){
+bool getPreprocArgs(int argc, char *argv[], vector<char *>& preproc_args, char object_file[]){
     bool postgcc = false; // track if you've seen the gcc command yet
     bool outflag = false; // track the -o outflag in gcc command
     bool isLinking = true;
-    int object_index;
+    int object_index = -1;
     for(int i=0; i< argc; i++){
         preproc_args[postgcc ? i+1 : i ] = argv[i];
 
@@ -56,22 +58,25 @@ bool getPreprocArgs(int argc, char *argv[], char*preproc_args[], char object_fil
 
     }
     preproc_args[argc+1] = '\0';
+
     return isLinking;
 }
 
 int main(int argc, char *argv[]){
 
     if (argc < 2 || strcmp(argv[1], COMPILER) != 0){
-        std::cout << "This is a wrapper for gcc. It can only be called before gcc." << std::endl;
+        cout << "This is a wrapper for gcc. It can only be called before gcc." << endl;
         exit(-1); // TODO: Replace with real error code
     }
     int status = 0;
     char object_file[50];
 
-    char *preproc_args[argc + 2];
+    // char *preproc_args[argc + 2];
+   vector<char *> preproc_args( argc+2);
     bool isLinking = getPreprocArgs(argc, argv, preproc_args, object_file);
 
-    std::chrono::high_resolution_clock::time_point t0 = std::chrono::high_resolution_clock::now();
+
+    chrono::high_resolution_clock::time_point t0 = chrono::high_resolution_clock::now();
 
     int pid = fork();
 
@@ -79,9 +84,9 @@ int main(int argc, char *argv[]){
 
         if (verbose) {
             for (int i = 1; i <= argc + 1; i++) {
-                std::cout << preproc_args[i] << " ";
+                cout << preproc_args[i] << " ";
             }
-            std::cout << std::endl;
+            cout << std::endl;
         }
         if(!isLinking) {
             execvp(argv[1], (char *const *) &preproc_args[1]);
@@ -92,16 +97,16 @@ int main(int argc, char *argv[]){
     else{  // parent
         wait(&status);
         if (!WIFEXITED(status) || WEXITSTATUS(status) != 0) {
-            std::cerr << "Process  (pid " << pid << ") failed" << std::endl;
+            std::cerr << "Preprocess process (pid " << pid << ") failed" << std::endl;
             exit(1);
         }
-        std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
-        auto dif = std::chrono::duration_cast<std::chrono::microseconds>( t1 - t0 ).count();
+        chrono::high_resolution_clock::time_point t1 = chrono::high_resolution_clock::now();
+        auto dif = chrono::duration_cast<chrono::microseconds>( t1 - t0 ).count();
         if(verbose){
             if(!isLinking){
-                std::cout << "PREPROCTIME " <<  dif << std::endl;
+                cout << endl << "PREPROCTIME " <<  dif << endl;
             } else {
-                std::cout << "LINKINGTIME " <<  dif << std::endl;
+                cout << endl << "LINKINGTIME " <<  dif << endl;
             }
         }
     }
@@ -112,7 +117,7 @@ int main(int argc, char *argv[]){
     COMPILE_COMMAND[OUTINDEX] = object_file;
     COMPILE_COMMAND[SRCINDEX] = srcfile;
 
-    std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
+    chrono::high_resolution_clock::time_point t2 = chrono::high_resolution_clock::now();
 
     pid = fork();
 
@@ -124,9 +129,9 @@ int main(int argc, char *argv[]){
 
         if (verbose) {
             for (int i = 0; i < 7; i++) {
-                std::cout << COMPILE_COMMAND[i] << " ";
+                cout << COMPILE_COMMAND[i] << " ";
             }
-            std::cout << std::endl;
+            cout << endl;
         }
 
         execvp ("gcc", (char* const*)&COMPILE_COMMAND);
@@ -134,21 +139,24 @@ int main(int argc, char *argv[]){
     else{  // parent
         wait(&status);
         if (!WIFEXITED(status) || WEXITSTATUS(status) != 0) {
-            std::cerr << "Process  (pid " << pid << ") failed" << std::endl;
+            std::cerr << "Compile process  (pid " << pid << ") failed" << std::endl;
             exit(1);
         }
-        std::chrono::high_resolution_clock::time_point t3 = std::chrono::high_resolution_clock::now();
-        auto dif = std::chrono::duration_cast<std::chrono::microseconds>( t3 - t2 ).count();
+        chrono::high_resolution_clock::time_point t3 = chrono::high_resolution_clock::now();
+        auto dif = chrono::duration_cast<chrono::microseconds>( t3 - t2 ).count();
         if(!isLinking && verbose){
-            std::cout << "COMPILETIME " <<  dif << std::endl;
+            cout << "COMPILETIME " <<  dif << endl;
         }
     }
 
     if(!isLinking){
         std::stringstream ss;
         ss << "rm " << COMPILE_COMMAND[SRCINDEX];
-        std::cout << ss.str() << std::endl;
-        system(ss.str().c_str());
+        cout << ss.str() << endl;
+        int ret = system(ss.str().c_str());
+        if (ret != 0){
+            cerr << "Error occurred deleting file: " << ss.str() << endl;
+        }
     }
 
 }
