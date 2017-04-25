3f80f4889b2f7fee836983046334ea34c7d74049
Sadjad Fouladi <sfouladi@gmail.com>
2017-04-25 11:48:07 -0700

    Create skeleton for gg-sandbox.

---
+++ src/frontend/gg-sandbox.cc
@@ -0,0 +1,34 @@
+/* -*-mode:c++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+
+#include <iostream>
+
+#include "exception.hh"
+#include "traced_process.hh"
+#include "thunk.hh"
+
+using namespace std;
+
+void usage( const char * argv0 )
+{
+  cerr << argv0 << endl;
+}
+
+int main( int argc, char const * argv[] )
+{
+  try {
+    if ( argc <= 0 ) {
+      abort();
+    }
+
+    if ( argc != 1 ) {
+      usage( argv[ 0 ] );
+      return EXIT_FAILURE;
+    }
+  }
+  catch ( const exception &  e ) {
+    print_exception( argv[ 0 ], e );
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
+}
