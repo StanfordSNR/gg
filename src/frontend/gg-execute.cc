05e8c7b411a9477f0fac69591a13e51fb441c3d8
Sadjad Fouladi <sfouladi@gmail.com>
2017-05-10 16:19:54 -0700

    Add the option to execute the thunk in a sandbox.

--- src/frontend/gg-execute.cc
+++ src/frontend/gg-execute.cc
@@ -2,19 +2,21 @@
 
 #include <unistd.h>
 #include <cstring>
+#include <getopt.h>
 #include <iostream>
 
 #include "exception.hh"
 #include "thunk.hh"
 #include "thunk_reader.hh"
 #include "child_process.hh"
+#include "sandbox.hh"
 
 using namespace std;
 using namespace gg::thunk;
 
 void usage( const char * argv0 )
 {
-  cerr << argv0 << " THUNK" << endl;
+  cerr << argv0 << "[--sandboxed,-s] [--gg-dir, -g=<arg>] THUNK" << endl;
 }
 
 int main( int argc, char * argv[] )
@@ -24,21 +26,61 @@ int main( int argc, char * argv[] )
       abort();
     }
 
-    if ( argc != 2 ) {
+    if ( argc < 2 ) {
       usage( argv[ 0 ] );
       return EXIT_FAILURE;
     }
 
-    ThunkReader thunk_reader { argv[ 1 ] };
-    Thunk thunk = thunk_reader.read_thunk();
+    bool sandboxed = false;
+    string gg_dir = ".gg/";
 
-    ChildProcess process { thunk.outfile(), [&]() { return thunk.execute(); } };
+    const option command_line_options[] = {
+      { "sandboxed", no_argument,       nullptr, 's' },
+      { "gg-dir",    required_argument, nullptr, 'g' },
+    };
 
-    while ( not process.terminated() ) {
-      process.wait();
+    while ( true ) {
+      const int opt = getopt_long( argc, argv, "sg:", command_line_options, nullptr );
+
+      if ( opt == -1 ) {
+        break;
+      }
+
+      switch ( opt ) {
+      case 's': sandboxed = true; break;
+      case 'g': gg_dir = optarg; break;
+      default:
+        throw runtime_error( "invalid option" );
+      }
     }
 
-    cerr << "Exited: " << process.exit_status() << endl;
+    if ( optind >= argc ) {
+      usage( argv[ 0 ] );
+      return EXIT_FAILURE;
+    }
+
+    ThunkReader thunk_reader { argv[ optind ] };
+    Thunk thunk = thunk_reader.read_thunk();
+
+    if ( not sandboxed ) {
+      ChildProcess process { thunk.outfile(), [&]() { return thunk.execute(); } };
+
+      while ( not process.terminated() ) {
+        process.wait();
+      }
+
+      cerr << "Exited: " << process.exit_status() << endl;
+    }
+    else {
+      unordered_map<string, Permissions> allowed_files;
+
+      for ( const InFile & infile : thunk.infiles() ) {
+        allowed_files[ gg_dir.append( infile.hash() ) ] = { true, true, true };
+      }
+
+      SandboxedProcess process { [&]() { return thunk.execute(); }, allowed_files };
+      process.execute();
+    }
   }
   catch ( const exception &  e ) {
     print_exception( argv[ 0 ], e );
