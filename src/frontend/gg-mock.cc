d0a5aa0a6186a4f032b1ccb477d94dd1215b8433
Sadjad Fouladi <sfouladi@gmail.com>
2017-05-22 14:25:12 -0700

    Create skeleton for gg-mock.

---
+++ src/frontend/gg-mock.cc
@@ -0,0 +1,32 @@
+/* -*-mode:c++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+
+#include <iostream>
+
+#include "exception.hh"
+
+using namespace std;
+
+void usage( const char * argv0 )
+{
+  cerr << argv0 << " COMMAND [option]..." << endl;
+}
+
+int main( int argc, char * argv[] )
+{
+  try {
+    if ( argc <= 0 ) {
+      abort();
+    }
+
+    if ( argc < 2 ) {
+      usage( argv[ 0 ] );
+      return EXIT_FAILURE;
+    }
+  }
+  catch ( const exception &  e ) {
+    print_exception( argv[ 0 ], e );
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
+}
