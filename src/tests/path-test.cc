bdbc3d6917431a76129ad8cc4f433e24a4820f87
Sadjad Fouladi <sfouladi@gmail.com>
2017-07-26 16:59:35 -0700

    path-test.cc: Add tests for `path::lexically_normal`.

---
+++ src/tests/path-test.cc
@@ -0,0 +1,58 @@
+/* -*-mode:c++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+
+#include <stdexcept>
+
+#include "path.hh"
+
+using namespace std;
+
+void test_lexically_normal( const string & pathname, const string & expected_path )
+{
+  string normalized_path = roost::path( pathname ).lexically_normal().string();
+  if ( expected_path != normalized_path ) {
+    throw runtime_error( "lexically_normal() test failed: " + pathname );
+  }
+}
+
+int main()
+{
+  test_lexically_normal( "/", "/" );
+
+  test_lexically_normal( "/home/sadjad", "/home/sadjad" );
+
+  test_lexically_normal( "./home/sadjad", "home/sadjad" );
+
+  test_lexically_normal( "///home////////sadja....d///////", "/home/sadja....d" );
+
+  test_lexically_normal( "/../home/sadjad", "/home/sadjad" );
+
+  test_lexically_normal( "/home/../sadjad//john", "/sadjad/john" );
+
+  test_lexically_normal(
+    "/home/sadjad/../../usr/bin/../lib/gcc/../ncurses/./././../g++/./stdlib.so",
+    "/usr/lib/g++/stdlib.so" );
+
+  test_lexically_normal(
+    "/home/sadjad/../../../././../../././../../../../home/.././etc/john/sadjad",
+    "/etc/john/sadjad" );
+
+  test_lexically_normal(
+    "../../../home/../etc/../dev/../../proc/cpuinfo",
+    "../../../../proc/cpuinfo" );
+
+  test_lexically_normal( "../../../../../..", "../../../../../.." );
+
+  test_lexically_normal(
+    "/home/sadjad/projects/gg/././../../../../../../../usr/bin/./gcc",
+    "/usr/bin/gcc" );
+
+  test_lexically_normal( "./home/sadjad/.", "home/sadjad" );
+
+  test_lexically_normal( "./home/sadjad//..", "home" );
+
+  test_lexically_normal(
+    "../../../home/../../../../../../../../../dev/video0",
+    "../../../../../../../../../../../dev/video0" );
+
+  return 0;
+}
