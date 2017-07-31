410c6c2514407126739a6c117c8d2480870afee2
Sadjad Fouladi <sfouladi@gmail.com>
2017-07-31 10:53:18 -0700

    temp_dir.hh: Create UniqueDirectory class.

---
+++ src/util/temp_dir.hh
@@ -0,0 +1,34 @@
+/* -*-mode:c++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+
+#ifndef TEMP_DIR_HH
+#define TEMP_DIR_HH
+
+#include <vector>
+#include <string>
+
+class UniqueDirectory
+{
+private:
+  std::vector<char> mutable_temp_dirname_;
+
+protected:
+  bool moved_away_;
+
+public:
+  UniqueDirectory( const std::string & dirname_template );
+  virtual ~UniqueDirectory() {}
+
+  std::string name( void ) const;
+
+  /* ban copying */
+  UniqueDirectory( const UniqueDirectory & other ) = delete;
+  UniqueDirectory & operator=( const UniqueDirectory & other ) = delete;
+
+  /* allow move constructor */
+  UniqueDirectory( UniqueDirectory && other );
+
+  /* ... but not move assignment operator */
+  UniqueDirectory & operator=( UniqueDirectory && other ) = delete;
+};
+
+#endif /* TEMP_DIR_HH */
