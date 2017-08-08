ca5b7b67f4d9407a94629e1b97e040a958e27fc5
Sadjad Fouladi <sfouladi@gmail.com>
2017-08-08 11:03:28 -0700

    placeholder.cc: Add order and size to the placeholder.

--- src/thunk/placeholder.cc
+++ src/thunk/placeholder.cc
@@ -12,9 +12,12 @@ const string SHEBANG_DIRECTIVE { "#!/usr/bin/env gg-reduce" };
 const string EXEC_FLAG { " -e" };
 
 ThunkPlaceholder::ThunkPlaceholder( const string & hash,
+                                    const size_t order,
+                                    const off_t size,
                                     const bool execute_after_force )
 
-  : hash_( hash ), execute_after_force_( execute_after_force )
+  : hash_( hash ), order_( order ), size_( size ),
+    execute_after_force_( execute_after_force )
 {}
 
 void ThunkPlaceholder::write( const string & filename ) const
@@ -23,7 +26,7 @@ void ThunkPlaceholder::write( const string & filename ) const
   fout << SHEBANG_DIRECTIVE
        << ( execute_after_force_ ? EXEC_FLAG : string( "" ) )
        << endl
-       << hash_
+       << hash_ << " " << order_ << " "  << size_
        << endl;
 
   roost::chmod( filename, 755 );
@@ -31,9 +34,8 @@ void ThunkPlaceholder::write( const string & filename ) const
 
 Optional<ThunkPlaceholder> ThunkPlaceholder::read( const string & filename )
 {
-  string hash;
   bool execute_after_force = false;
-
+  
   ifstream fin { filename };
   string line;
   getline( fin, line );
@@ -45,6 +47,11 @@ Optional<ThunkPlaceholder> ThunkPlaceholder::read( const string & filename )
     return {};
   }
 
-  getline( fin, hash );
-  return ThunkPlaceholder( line, execute_after_force );
+  string hash;
+  size_t order;
+  off_t size;
+
+  fin >> hash >> order >> size;
+
+  return ThunkPlaceholder( hash, order, size, execute_after_force );
 }
--- src/thunk/placeholder.hh
+++ src/thunk/placeholder.hh
@@ -3,6 +3,7 @@
 #ifndef PLACEHOLDER_HH
 #define PLACEHOLDER_HH
 
+#include <sys/types.h>
 #include <string>
 
 #include "optional.hh"
@@ -11,12 +12,20 @@ class ThunkPlaceholder
 {
 private:
   std::string hash_;
+  size_t order_;
+  off_t size_;
+
   bool execute_after_force_;
 
 public:
-  ThunkPlaceholder( const std::string & hash, const bool execute_after_force );
+  ThunkPlaceholder( const std::string & hash,
+                    const size_t order,
+                    const off_t size,
+                    const bool execute_after_force );
 
   std::string hash() const { return hash_; }
+  size_t order() const { return order_; }
+  off_t size() const { return size_; }
 
   void write( const std::string & filename ) const;
   static Optional<ThunkPlaceholder> read( const std::string & filename );
