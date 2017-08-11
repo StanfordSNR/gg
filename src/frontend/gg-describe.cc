e538b952ede1d266bc6c050a1f1298572e926c3e
Sadjad Fouladi <sfouladi@gmail.com>
2017-08-11 12:12:12 -0700

    gg-describe.cc: Add switch to print out executable hash

--- src/frontend/gg-describe.cc
+++ src/frontend/gg-describe.cc
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <cstring>
 #include <iostream>
+#include <getopt.h>
 #include <google/protobuf/util/json_util.h>
 
 #include "exception.hh"
@@ -16,7 +17,7 @@ using namespace gg::thunk;
 
 void usage( const char * argv0 )
 {
-  cerr << argv0 << " THUNK" << endl;
+  cerr << argv0 << " [--executable-hash, -e] THUNK" << endl;
 }
 
 int main( int argc, char * argv[] )
@@ -26,22 +27,51 @@ int main( int argc, char * argv[] )
       abort();
     }
 
-    if ( argc != 2 ) {
+    if ( argc < 2 ) {
       usage( argv[ 0 ] );
       return EXIT_FAILURE;
     }
 
-    ThunkReader thunk_reader { argv[ 1 ] };
+    bool print_executable_hash = false;
+
+    const option command_line_options[] = {
+      { "executable-hash", no_argument, nullptr, 'e' },
+      { 0, 0, 0, 0 }
+    };
+
+    while ( true ) {
+      const int opt = getopt_long( argc, argv, "e", command_line_options, nullptr );
+
+      if ( opt == -1 ) { break; }
+
+      switch ( opt ) {
+      case 'e':
+        print_executable_hash = true;
+        break;
+      }
+    }
+
+    if ( optind >= argc ) {
+      usage( argv[ 0 ] );
+      return EXIT_FAILURE;
+    }
+
+    ThunkReader thunk_reader { argv[ optind ] };
     Thunk thunk = thunk_reader.read_thunk();
 
-    string textf;
-    JsonPrintOptions options;
-    options.add_whitespace = true;
-    options.always_print_primitive_fields = true;
-    MessageToJsonString( thunk.to_protobuf(), &textf, options );
+    if ( print_executable_hash ) {
+      cout << thunk.executable_hash() << endl;
+    }
+    else {
+      string textf;
+      JsonPrintOptions options;
+      options.add_whitespace = true;
+      options.always_print_primitive_fields = true;
+      MessageToJsonString( thunk.to_protobuf(), &textf, options );
 
-    cout << "// [order=" << thunk.order() << "]" << endl;
-    cout << textf << endl;
+      cout << "// [order=" << thunk.order() << "]" << endl;
+      cout << textf << endl;
+    }
   }
   catch ( const exception &  e ) {
     print_exception( argv[ 0 ], e );
