ca5eb1b733875b102b15316906f5fae14d57909d
Sadjad Fouladi <sfouladi@gmail.com>
2017-08-14 15:36:42 -0700

    Create graph.hh

---
+++ src/thunk/graph.hh
@@ -0,0 +1,14 @@
+/* -*-mode:c++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+
+#ifndef GRAPH_HH
+#define GRAPH_HH
+
+class DependencyGraph
+{
+private:
+
+public:
+  DependencyGraph();
+};
+
+#endif /* GRAPH_HH */
