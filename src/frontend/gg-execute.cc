8ab1dd50bda6708cc08bd5a4692a192ebddcda86
Sadjad Fouladi <sfouladi@gmail.com>
2017-08-16 15:08:22 -0700

    Implement gg-execute functionality.

--- src/frontend/gg-execute.cc
+++ src/frontend/gg-execute.cc
@@ -1,10 +1,101 @@
 /* -*-mode:c++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
 
 #include <iostream>
+#include <string>
 
 #include "exception.hh"
+#include "thunk.hh"
+#include "path.hh"
+#include "ggpaths.hh"
+#include "child_process.hh"
+#include "temp_file.hh"
+#include "temp_dir.hh"
+#include "thunk_reader.hh"
 
 using namespace std;
+using namespace gg::thunk;
+
+const roost::path gg_path = gg::paths::blobs();
+const roost::path gg_reductions_path = gg::paths::reductions();
+
+const bool sandboxed = ( getenv( "GG_SANDBOXED" ) != NULL );
+const string temp_dir_template = "/tmp/thunk-execute";
+const string temp_file_template = "/tmp/thunk-file";
+
+void store_thunk_reduction( const string & original_hash,
+                            const string & new_hash )
+{
+  roost::symlink( new_hash, gg_reductions_path / original_hash );
+}
+
+string execute_thunk( const Thunk & thunk, const roost::path & thunk_path )
+{
+  assert( thunk.order() == 1 );
+
+  /* when executing the thunk, we create a temp directory, and execute the thunk
+     in that directory. then we take the outfile, compute the hash, and move it
+     to the .gg directory. */
+
+  // PREPARING THE ENV
+  TempDirectory exec_dir { temp_dir_template };
+  roost::path exec_dir_path { exec_dir.name() };
+
+  roost::path outfile_dir = roost::dirname( thunk.outfile() );
+  roost::create_directories( outfile_dir );
+
+  // EXECUTING THE THUNK
+  if ( not sandboxed ) {
+    ChildProcess process {
+      thunk.outfile(),
+      [thunk, thunk_path, exec_dir_path, &outfile_dir]() {
+        CheckSystemCall( "chdir", chdir( exec_dir_path.string().c_str() ) );
+        roost::create_directories( outfile_dir );
+        return thunk.execute( gg_path, thunk_path );
+      }
+    };
+
+    while ( not process.terminated() ) {
+      process.wait();
+    }
+
+    if ( process.exit_status() != 0 ) {
+      throw runtime_error( "thunk execution failed: " + thunk_path.string() );
+    }
+  }
+  else {
+    auto allowed_files = thunk.get_allowed_files( gg_path, thunk_path );
+
+    SandboxedProcess process {
+      allowed_files,
+      [thunk, thunk_path]() {
+        return thunk.execute( gg_path, thunk_path );
+      },
+      [exec_dir_path, &outfile_dir] () {
+        CheckSystemCall( "chdir", chdir( exec_dir_path.string().c_str() ) );
+        roost::create_directories( outfile_dir );
+      }
+    };
+
+    process.execute();
+
+    if ( not process.exit_status().initialized() or process.exit_status().get() != 0 ) {
+      throw runtime_error( "thunk execution failed: " + thunk_path.string() );
+    }
+  }
+
+  // GRABBING THE OUTPUT
+  roost::path outfile { exec_dir_path / thunk.outfile() };
+  string outfile_hash = InFile::compute_hash( outfile.string() );
+  roost::path outfile_gg = gg::paths::blob_path( outfile_hash );
+
+  if ( not roost::exists( outfile_gg ) ) {
+    roost::move_file( outfile, outfile_gg );
+  }
+
+  roost::remove( outfile );
+
+  return outfile_hash;
+}
 
 void usage( const char * argv0 )
 {
@@ -22,6 +113,13 @@ int main( int argc, char * argv[] )
       usage( argv[ 0 ] );
       return EXIT_FAILURE;
     }
+
+    string thunk_hash { argv[ 1 ] };
+    roost::path thunk_path = gg::paths::blob_path( thunk_hash );
+    ThunkReader thunk_reader { thunk_path.string() };
+    Thunk thunk = thunk_reader.read_thunk();
+    string output_hash = execute_thunk( thunk, thunk_path );
+    store_thunk_reduction( thunk_hash, output_hash );
 
     return EXIT_SUCCESS;
   }
