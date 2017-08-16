9afe54581609b2e97801792065e4ff356b05c627
Sadjad Fouladi <sfouladi@gmail.com>
2017-08-16 11:34:05 -0700

    graph.hh: Create function to add a thunk to the graph

--- src/thunk/graph.cc
+++ src/thunk/graph.cc
@@ -1,3 +1,53 @@
 /* -*-mode:c++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
 
 #include "graph.hh"
+
+#include <stdexcept>
+
+#include "paths.hh"
+#include "thunk_reader.hh"
+
+using namespace std;
+using namespace gg::thunk;
+
+DependencyGraph::DependencyGraph()
+{}
+
+size_t DependencyGraph::add_thunk( const string & hash )
+{
+  if ( hash_to_id_.count( hash ) > 0 ) {
+    const size_t thunk_id = hash_to_id_[ hash ];
+    if ( thunks_.count( thunk_id ) > 0 ) {
+      return thunk_id; // already added
+    }
+    else {
+      throw runtime_error( "cycle detected while adding thunk: " + hash );
+    }
+  }
+
+  ThunkReader thunk_reader { gg::paths::blob_path( hash ).string() };
+  Thunk thunk = thunk_reader.read_thunk();
+
+  const size_t thunk_id = insert_thunk_hash( hash );
+
+  for ( const InFile & infile : thunk.infiles() ) {
+    if ( infile.order() > 0 ) {
+      size_t infile_thunk_id = add_thunk( infile.content_hash() );
+      referenced_thunks_[ infile_thunk_id ].insert( thunk_id );
+    }
+  }
+
+  thunks_.emplace( make_pair( thunk_id, move( thunk ) ) );
+
+  return thunk_id;
+}
+
+size_t DependencyGraph::insert_thunk_hash( const string & hash )
+{
+  unique_lock<mutex> lock { unique_id_mutex_ };
+
+  static size_t auto_increment_id = 0;
+  hash_to_id_.insert( { hash, auto_increment_id } );
+  id_to_hash_.insert( { auto_increment_id, hash } );
+  return auto_increment_id++;
+}
--- src/thunk/graph.hh
+++ src/thunk/graph.hh
@@ -3,12 +3,33 @@
 #ifndef GRAPH_HH
 #define GRAPH_HH
 
+#include <string>
+#include <memory>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <mutex>
+
+#include "thunk.hh"
+
 class DependencyGraph
 {
 private:
+  std::mutex unique_id_mutex_ {};
+
+  std::unordered_map<std::string, size_t> hash_to_id_ {};
+  std::map<size_t, std::string> id_to_hash_ {};
+
+  std::map<size_t, gg::thunk::Thunk> thunks_ {};
+  std::map<size_t, std::set<size_t>> referenced_thunks_ {};
+
+  size_t insert_thunk_hash( const std::string & hash );
 
 public:
   DependencyGraph();
+
+  size_t add_thunk( const std::string & hash );
 };
 
 #endif /* GRAPH_HH */
