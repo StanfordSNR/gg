6996c594558a0c23333c9e65f53b869f4b3cc7b3
Sadjad Fouladi <sfouladi@gmail.com>
2017-08-22 17:00:58 -0700

    s3.py: Create class to generate S3 authentication headers

---
+++ src/remote/s3.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3.6
+
+import hmac
+import hashlib
+import datetime
+import boto3
+import requests
+
+from base64 import b64encode
+from email.utils import formatdate
+
+def sign(key, msg):
+    return b64encode(hmac.new(key, msg.encode('utf-8'), hashlib.sha1).digest()).decode('ascii')
+
+class S3AuthGeneator:
+    def __init__(self, aws_access_key, aws_secret_access_key):
+        self.aws_access_key = aws_access_key.encode('ascii')
+        self.aws_secret_access_key = aws_secret_access_key.encode('ascii')
+        self.service_name = 's3'
+
+    def __call__(self, method, bucket_name, object_name, headers):
+        current_time = datetime.datetime.utcnow()
+        timestamp = formatdate(timeval=None, localtime=False, usegmt=True)
+
+        canonical_uri = '/{bucket_name}/{object_name}'.format(
+            bucket_name=bucket_name, object_name=object_name)
+
+        string_to_sign = method + '\n' + \
+            '\n' + \
+            'application/octet-stream' + '\n' + \
+            timestamp + '\n'
+
+        candidate_headers = []
+        for hk, hv in headers.items():
+            if hk.lower().startswith("x-amz-"):
+                candidate_headers += [hk]
+        candidate_headers.sort()
+
+        for hk in candidate_headers:
+            string_to_sign += hk.lower() + ':' + headers[hk] + '\n'
+
+        string_to_sign += canonical_uri
+
+        headers['Date'] = timestamp
+        headers['Authorization'] = "AWS {}:{}".format(self.aws_access_key.decode('ascii'),
+            sign(self.aws_secret_access_key, string_to_sign))
+
+if __name__ == '__main__':
+    data = b'YEAH, IT WORKS!'
+    creds = boto3.Session().get_credentials()
+    auth_gen = S3AuthGeneator(creds.access_key, creds.secret_key)
+
+    bucket_name = 'gg-us-west-2'
+    object_name = 'test'
+    url = 'https://{}.s3.amazonaws.com/{}'.format(bucket_name, object_name)
+
+    headers = {
+        'Content-Type': 'application/octet-stream',
+        'x-amz-acl': 'public-read',
+        'x-amz-tagging': 'gg=file'
+    }
+
+    auth_gen('PUT', bucket_name, object_name, headers)
+    response = requests.put(url, data=data, headers=headers)
+    print(response.status_code)
