2afd614a26a4ca16e72eaae2bc768a8beb7eacfd
Sadjad Fouladi <sfouladi@gmail.com>
2017-08-26 17:43:59 -0700

    gg-lambda-execute: Print the total size of the dependencies.

--- src/remote/common.py
+++ src/remote/common.py
@@ -2,6 +2,13 @@ import os
 import stat
 import subprocess as sub
 
+def sizeof_fmt(num, suffix='B'):
+    for unit in ['','K','M','G','T','P','E','Z']:
+        if abs(num) < 1024.0:
+            return "%3.1f %s%s" % (num, unit, suffix)
+        num /= 1024.0
+    return "%.1f %s%s" % (num, 'Y', suffix)
+
 def is_executable(path):
     st = os.stat(path)
     return ( st.st_mode & stat.S_IEXEC ) != 0
