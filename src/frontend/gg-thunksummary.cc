8a4bfd57929d2a504f33845a13b2fb5bf765cb8d
Keith Winstein <keithw@cs.stanford.edu>
2017-08-31 03:19:52 -0700

    gg-thunksummary: track cumulative totals (files, thunks)

--- src/frontend/gg-thunksummary.cc
+++ src/frontend/gg-thunksummary.cc
@@ -24,23 +24,58 @@ string shortn( const string & hash )
   return hash.substr( 0, 5 );
 }
 
-void print_thunk_info( const string & hash, unsigned int indent )
+struct ThunkStats
+{
+  std::unordered_map<std::string, size_t> files {};
+  std::unordered_set<std::string> thunks {};
+
+  ThunkStats & operator+=( const ThunkStats & other )
+  {
+    files.insert( other.files.begin(), other.files.end() );
+    thunks.insert( other.thunks.begin(), other.thunks.end() );
+    return *this;
+  }
+
+  size_t total_file_size() const
+  {
+    size_t ret = 0;
+    for ( const auto & x : files ) {
+      ret += x.second;
+    }
+    return ret;
+  }
+};
+
+ThunkStats print_thunk_info( const string & hash, unsigned int indent )
 {
   const Thunk thunk { ThunkReader { gg::paths::blob_path( hash ).string() }.read_thunk() };
   const string indentation( indent, ' ' );
   
   const string display_name = roost::rbasename( thunk.outfile() ).string() + " (" + shortn( hash ) + ")";
 
-  cout << indentation << display_name << " {" << thunk.function().exe() << "}\n";
+  ThunkStats stats;
+  stats.thunks.insert( hash );
+
+  cout << indentation << display_name << " {" << roost::rbasename( thunk.function().exe() ).string() << "}\n";
 
   for ( const auto & infile : thunk.infiles() ) {
     const string infile_name = roost::rbasename( infile.filename() ).string() + " (" + shortn( infile.content_hash() ) + ")";
     if ( infile.order() ) {
-      print_thunk_info( infile.content_hash(), indent + 1 );
+      stats += print_thunk_info( infile.content_hash(), indent + 1 );
     } else if ( infile.size() ) {
       cout << indentation << " " << infile_name << "\n";
+
+      stats.files.insert( { infile.content_hash(), infile.size() } );
     }
   }
+
+  const string plural = stats.thunks.size() > 1 ? "s" : "";
+
+  cout << indentation << display_name << " uses " << stats.thunks.size()
+       << " thunk" << plural << " and " << stats.files.size() << " files totaling "
+       << stats.total_file_size() / 1048576 << " MiB\n";
+
+  return stats;
 }
 
 int main( int argc, char * argv[] )
