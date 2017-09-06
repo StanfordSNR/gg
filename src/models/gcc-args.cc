09cec0a1938125438bf4c292cfc976ac9d3b4dd3
Sadjad Fouladi <sfouladi@gmail.com>
2017-09-05 18:36:19 -0700

    model-gcc.cc: Add support for -mregparm option.

--- src/models/gcc-args.cc
+++ src/models/gcc-args.cc
@@ -101,6 +101,8 @@ GCCArguments::GCCArguments( const int argc, char ** argv )
     { GCCOption::mtune,   "mtune",   required_argument, false, '=' },
 
     { GCCOption::mpreferred_stack_boundary, "mpreferred-stack-boundary", required_argument, false, '=' },
+
+    { GCCOption::mregparm, "mregparm", required_argument, false, '=' },
   };
 
   constexpr size_t option_count = sizeof( gcc_options_data ) / sizeof( GCCOptionData);
