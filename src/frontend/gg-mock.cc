98b24595382a1eed8ebcf91eb0f09d6d81800133
Keith Winstein <keithw@cs.stanford.edu>
2017-09-07 15:32:16 -0700

    Rewrite thunk observation logic in gg-mock

--- src/frontend/gg-mock.cc
+++ src/frontend/gg-mock.cc
@@ -46,6 +46,7 @@ int main( int argc, char * argv[] )
             invocation->fetch_arguments();
             if ( invocation->arguments()->at( 0 ).value<int>() == 0 and
                  invocation->arguments()->at( 1 ).value<int>() == 0x03031990 ) {
+              cerr << "detaching!" << endl;
               tcb.detach();
             }
 
@@ -55,20 +56,37 @@ int main( int argc, char * argv[] )
             {
               invocation->fetch_arguments();
               string open_path = invocation->arguments()->at( 0 ).value<string>();
+              cerr << tcb.to_string() << endl;
 
-              int fd_num = open( open_path.c_str(), O_RDONLY );
-
-              if ( fd_num < 0 or isatty( fd_num ) ) {
-                return;
+              /* if process wants to truncate the file, we don't
+                 care whether it's a thunk placeholder or not */
+              const int open_flags = invocation->arguments()->at( 1 ).value<int>();
+              if ( open_flags & O_TRUNC ) {
+                break;
               }
 
-              FileDescriptor fd { fd_num };
+              /* otherwise: does the file
+                 (1) exist,
+                 (2) as a regular file,
+                 (3) that we can open,
+                 (4) and as a thunk placeholder? */
 
-              if ( ThunkPlaceholder::is_placeholder( move( fd ) ) ) {
-                throw runtime_error( "Somebody tried to open a thunk: " + open_path );
+              struct stat stat_buf;
+              const int stat_ret = stat( open_path.c_str(), &stat_buf );
+              if ( stat_ret == 0 ) {
+                /* (1) it exists -- is it a regular file? */
+                if ( S_ISREG( stat_buf.st_mode ) ) {
+                  /* (2) it's a regular file -- can we open it? */
+                  const int fd_num = open( open_path.c_str(), O_RDONLY );
+                  if ( fd_num >= 0 ) {
+                    /* (3) successfully opened -- is it a thunk placeholder? */
+                    if ( ThunkPlaceholder::is_placeholder( FileDescriptor { fd_num } ) ) {
+                      throw runtime_error( "Attempt to open thunk placeholder: " + open_path );
+                    }
+                  }
+                }
               }
             }
-
             break;
           }
         },
