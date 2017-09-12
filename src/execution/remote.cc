ec1a703ec452a5c55b029aac035ffac90ae02171
Sadjad Fouladi <sfouladi@gmail.com>
2017-09-11 23:21:22 -0700

    remote.cc: Use libcajun instead of protobuf to write/read JSON.

--- src/execution/remote.cc
+++ src/execution/remote.cc
@@ -3,9 +3,12 @@
 #include "remote.hh"
 
 #include <string>
+#include <sstream>
 #include <crypto++/base64.h>
 #include <crypto++/files.h>
-#include <google/protobuf/util/json_util.h>
+#include <cajun/json/reader.h>
+#include <cajun/json/writer.h>
+#include <cajun/json/elements.h>
 
 #include "digest.hh"
 #include "thunk.hh"
@@ -18,7 +21,6 @@ using namespace std;
 using namespace gg;
 using namespace gg::thunk;
 using namespace CryptoPP;
-using namespace google::protobuf::util;
 using namespace lambda;
 
 RequestGenerator::RequestGenerator( const AWSCredentials & credentials,
@@ -35,28 +37,31 @@ HTTPRequest RequestGenerator::generate( const Thunk & thunk,
   FileSource s( gg::paths::blob_path( thunk_hash ).string().c_str(), true,
                 new Base64Encoder( new StringSink( base64_thunk ), false ) );
 
-  gg::protobuf::LambdaEvent lambda_event;
-  lambda_event.set_thunk_hash( thunk_hash );
-  lambda_event.set_s3_bucket( gg::remote::s3_bucket() );
-  lambda_event.set_thunk_data( base64_thunk );
+  json::Object lambda_event;
+  lambda_event[ "thunk_hash" ] = json::String( thunk_hash );
+  lambda_event[ "s3_bucket" ] = json::String( gg::remote::s3_bucket() );
+  lambda_event[ "thunk_data" ] = json::String( base64_thunk );
+
+  json::Array lambda_event_infiles;
 
   for ( const InFile & infile : thunk.infiles() ) {
     if ( infile.type() == InFile::Type::DUMMY_DIRECTORY ) {
       continue;
     }
 
-    protobuf::LambdaInFile * proto_infile = lambda_event.add_infiles();
-    proto_infile->set_hash( infile.content_hash() );
-    proto_infile->set_size( infile.size() );
-    proto_infile->set_executable( infile.type() == InFile::Type::EXECUTABLE );
+    json::Object event_infile;
+    event_infile[ "hash" ] = json::String( infile.content_hash() );
+    event_infile[ "size" ] = json::Number( infile.size() );
+    event_infile[ "executable" ] = json::Boolean(  infile.type() == InFile::Type::EXECUTABLE );
+
+    lambda_event_infiles.Insert( event_infile );
   }
 
-  string request_payload;
+  lambda_event[ "infiles" ] = lambda_event_infiles;
 
-  JsonPrintOptions options;
-  options.add_whitespace = false;
-  options.always_print_primitive_fields = true;
-  MessageToJsonString( lambda_event, &request_payload, options );
+  ostringstream oss;
+  json::Writer::Write( lambda_event, oss );
+  const string request_payload = oss.str();
 
   return LambdaInvocationRequest (
     credentials_, region_, function_name, request_payload,
@@ -96,15 +101,12 @@ void ExecutionConnectionManager::remove_connection( const std::string & hash )
 RemoteResponse::RemoteResponse( const string & response_str )
   : thunk_hash(), output_hash(), output_size(), is_executable()
 {
-  protobuf::RemoteResponse rr_proto;
+  istringstream iss { response_str };
+  json::Object response_json;
+  json::Reader::Read( response_json, iss );
 
-  JsonParseOptions options;
-  options.ignore_unknown_fields = true;
-
-  JsonStringToMessage( response_str, &rr_proto, options );
-
-  thunk_hash = rr_proto.thunk_hash();
-  output_hash = rr_proto.output_hash();
-  output_size = rr_proto.output_size();
-  is_executable = rr_proto.executable_output();
+  thunk_hash = static_cast<json::String>( response_json[ "thunk_hash" ] );
+  output_hash = static_cast<json::String>( response_json[ "output_hash" ] );
+  output_size = static_cast<json::Number>( response_json[ "output_size" ] );
+  is_executable = static_cast<json::Boolean>( response_json[ "executable_output" ] );
 }
