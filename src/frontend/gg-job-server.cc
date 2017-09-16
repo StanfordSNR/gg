d2249ee95f5229697b499581cbc4c5faf49f4061
Sadjad Fouladi <sfouladi@gmail.com>
2017-09-16 14:19:44 -0700

    gg-job-server: Create source file.

---
+++ src/frontend/gg-job-server.cc
@@ -0,0 +1,38 @@
+/* -*-mode:c++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+
+#include <iostream>
+#include <string>
+
+#include "exception.hh"
+
+using namespace std;
+
+void usage( const char * argv0 )
+{
+  cerr << argv0 << " PORT MAXJOBS" << endl;
+}
+
+int main( int argc, char * argv[] )
+{
+  try {
+    if ( argc <= 0 ) {
+      abort();
+    }
+
+    if ( argc != 3 ) {
+      usage( argv[ 0 ] );
+      return EXIT_FAILURE;
+    }
+
+    // string port { argv[ 1 ] };
+    // size_t max_jobs = stoul( argv[ 2 ] );
+
+    return EXIT_SUCCESS;
+  }
+  catch ( const exception &  e ) {
+    print_exception( argv[ 0 ], e );
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
+}
