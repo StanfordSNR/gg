a60a0019dd26a342a26d2b0f57c93277814afc8f
Sadjad Fouladi <sfouladi@gmail.com>
2017-09-17 12:56:46 -0700

    Let the job server take care of the toolchain.

--- src/remote/gg-job-server.py
+++ src/remote/gg-job-server.py
@@ -8,8 +8,23 @@ import time
 import json
 import http.server
 import socketserver
+import hashlib
+import base64
+import shutil
 
-import lambda_function.function
+import lambda_function.function as lambdafunc
+from lambda_function.ggpaths import GGPaths
+
+def gghash(filename, block_size=65536):
+    sha256 = hashlib.sha256()
+    size = 0
+
+    with open(filename, 'rb') as f:
+        for block in iter(lambda: f.read(block_size), b''):
+            size += len(block)
+            sha256.update(block)
+
+    return "{}{:08x}".format(base64.urlsafe_b64encode(sha256.digest()).decode('ascii').replace('=','').replace('-', '.'), size)
 
 class GGRequestHandler(http.server.BaseHTTPRequestHandler):
     def do_POST(self):
@@ -19,7 +34,7 @@ class GGRequestHandler(http.server.BaseHTTPRequestHandler):
         print(event['thunk_hash'])
 
         try:
-            output = lambda_function.function.handler(event, {})
+            output = lambdafunc.handler(event, {})
         except Exception as ex:
             output = {
                 'errorType': ex.__class__.__name__,
@@ -39,18 +54,29 @@ class GGServer(socketserver.ForkingMixIn, http.server.HTTPServer):
     pass
 
 def usage(argv0):
-    print("{} PORT".format(argv0))
+    print("{} PORT TOOLCHAIN-PATH".format(argv0))
 
 def main(port):
+    print('Starting server, listening on :{}'.format(port))
     gg_server = GGServer(('', port), GGRequestHandler)
     gg_server.serve_forever()
+
+def prepare_toolchain(toolchain_path):
+    for tbin in os.listdir(toolchain_path):
+        bin_path = os.path.join(toolchain_path, tbin)
+        bin_hash = gghash(bin_path)
+        shutil.copy(bin_path, GGPaths.blob_path(bin_hash))
 
 if __name__ == '__main__':
     if len(sys.argv) == 0:
         os.abort()
 
-    if len(sys.argv) != 2:
+    if len(sys.argv) != 3:
         usage(sys.argv[0])
         sys.exit(1)
+
+    print('Preparing toolchain... ', end='')
+    prepare_toolchain(sys.argv[2])
+    print('done.')
 
     main(int(sys.argv[1]))
