dd0caeb945e632c0a0cf5a7e12c79b37d1eeb105
Sadjad Fouladi <sfouladi@gmail.com>
2017-10-16 14:47:37 -0700

    engine.hh: Create ExecutionEngine skeleton.

--- src/execution/engine.hh
+++ src/execution/engine.hh
@@ -1,10 +1,27 @@
 /* -*-mode:c++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
 
+#ifndef ENGINE_HH
+#define ENGINE_HH
+
+#include <string>
+#include <functional>
+
 class ExecutionEngine
 {
-private:
 public:
-  ExecutionEngine();
+  typedef std::function<void( const std::string &, const std::string )> CallbackFunc;
 
-  
+private:
+  ExecutionLoop & exec_loop_
+
+  std::function<void( const std::string &, const std::string )> callback_;
+
+public:
+  ExecutionEngine( ExecutionLoop & loop, CallbackFunc callback )
+    : exec_loop_( loop ), callback_( callback )
+  {}
+
+  void force_thunk( const std::string & hash, const gg::thunk::Thunk & thunk );
 };
+
+#endif ENGINE_HH
