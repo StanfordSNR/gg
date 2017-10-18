767f959cf667cead2f20b47e74148f21a0eba68a
Sadjad Fouladi <sfouladi@gmail.com>
2017-10-18 14:53:29 -0700

    Create gg execution engine.

---
+++ src/execution/engine_gg.cc
@@ -0,0 +1,86 @@
+/* -*-mode:c++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+
+#include "engine_gg.hh"
+
+#include <stdexcept>
+
+#include "ggpaths.hh"
+#include "optional.hh"
+#include "system_runner.hh"
+#include "http_response.hh"
+#include "remote_response.hh"
+#include "units.hh"
+
+using namespace std;
+using namespace gg::thunk;
+
+HTTPRequest GGExecutionEngine::generate_request( const Thunk & thunk,
+                                                 const string & thunk_hash,
+                                                 const bool timelog )
+{
+  string payload = thunk.execution_payload( thunk_hash, timelog );
+  HTTPRequest request;
+  request.set_first_line( "POST /cgi-bin/gg/execute.cgi HTTP/1.1" );
+  request.add_header( HTTPHeader{ "Content-Length", to_string( payload.size() ) } );
+  request.add_header( HTTPHeader{ "Host", "gg-run-server" } );
+  request.done_with_headers();
+
+  request.read_in_body( payload );
+  assert( request.state() == COMPLETE );
+
+  return request;
+}
+
+void GGExecutionEngine::force_thunk( const string & hash,
+                                     const Thunk & thunk )
+{
+  HTTPRequest request = generate_request( thunk, hash, false );
+
+  TCPSocket socket;
+  socket.set_blocking( false );
+
+  try {
+    socket.connect( address_ );
+    throw runtime_error( "nonblocking connect unexpectedly succeeded immediately" );
+  } catch ( const unix_error & e ) {
+    if ( e.error_code() == EINPROGRESS ) {
+      /* do nothing */
+    } else {
+      throw;
+    }
+  }
+
+  exec_loop_.add_connection(
+    hash,
+    [this] ( const string & thunk_hash, const HTTPResponse & http_response )
+    {
+      running_jobs_--;
+
+      if ( http_response.status_code() != "200" ) {
+        throw runtime_error( "HTTP failure: " + http_response.status_code() );
+      }
+
+      RemoteResponse response = RemoteResponse::parse_message( http_response.body() );
+
+      if ( response.type != RemoteResponse::Type::SUCCESS ) {
+        throw runtime_error( "execution failed." );
+      }
+
+      if ( response.thunk_hash != thunk_hash ) {
+        cerr << http_response.str() << endl;
+        throw runtime_error( "expected output for " + thunk_hash + ", got output for " + response.thunk_hash );
+      }
+
+      gg::cache::insert( response.thunk_hash, response.output_hash );
+      callback_( response.thunk_hash, response.output_hash );
+    },
+    socket, request
+  );
+
+  running_jobs_++;
+}
+
+size_t GGExecutionEngine::job_count() const
+{
+  return running_jobs_;
+}
---
+++ src/execution/engine_gg.hh
@@ -0,0 +1,35 @@
+/* -*-mode:c++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+
+#ifndef ENGINE_GG_HH
+#define ENGINE_GG_HH
+
+#include "engine.hh"
+#include "thunk.hh"
+#include "http_request.hh"
+
+class GGExecutionEngine : public ExecutionEngine
+{
+private:
+  Address address_;
+
+  size_t running_jobs_ { 0 };
+
+  HTTPRequest generate_request( const gg::thunk::Thunk & thunk,
+                                const std::string & thunk_hash,
+                                const bool timelog );
+
+public:
+  GGExecutionEngine( const std::string & address, const uint16_t port,
+                     ExecutionLoop & loop, CallbackFunc callback )
+    : ExecutionEngine( loop, callback ), address_( address, port )
+  {}
+
+  void force_thunk( const std::string & hash, const gg::thunk::Thunk & thunk ) override;
+  size_t job_count() const override;
+
+  bool is_remote() const { return true; }
+  std::string label() const override { return "gg-remote"; }
+  bool can_execute( const gg::thunk::Thunk & ) const { return true; }
+};
+
+#endif /* ENGINE_GG_HH */
