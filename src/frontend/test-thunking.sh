14affb36bcb0df3f8e446e3fa9901dfa11a1f165
Sadjad Fouladi <sfouladi@gmail.com>
2017-12-06 16:57:02 -0800

    Remove unused files from frontend.

--- src/frontend/test-thunking.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-set -e
-
-make
-cd ../../../exploration/targets/make-4.2/
-rm remake.o || true
-../../../gg/src/frontend/gg-gcc gcc -DLOCALEDIR=\"/usr/local/share/locale\" -DLIBDIR=\"/usr/local/lib\" -DINCLUDEDIR=\"/usr/local/include\" -DHAVE_CONFIG_H -I.      -g -O2 -MT remake.o -MD -MP -MF .deps/remake.Tpo -c -o remake.o remake.c
-cd - > /dev/null
