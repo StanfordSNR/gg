73e5f15d21e14fa5119009c4c781694c3c175ab0
Sadjad Fouladi <sfouladi@gmail.com>
2018-03-16 22:24:24 -0700

    response.hh: Support multiple outputs.

---
+++ src/execution/response.cc
@@ -0,0 +1,50 @@
+/* -*-mode:c++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+
+#include "response.hh"
+
+#include <iostream>
+#include <stdexcept>
+#include <google/protobuf/util/json_util.h>
+
+#include "protobufs/gg.pb.h"
+
+using namespace std;
+using namespace gg;
+using namespace google::protobuf::util;
+
+ExecutionResponse ExecutionResponse::parse_message( const std::string & message )
+{
+  ExecutionResponse response;
+
+  JsonParseOptions parse_options;
+  gg::protobuf::ExecutionResponse response_proto;
+
+  if ( not JsonStringToMessage( message, &response_proto ).ok() ) {
+    cerr << "invalid response: " << message << endl;
+    response.status = JobStatus::OperationalFailure;
+    return response;
+  }
+
+  response.status = static_cast<JobStatus>( response_proto.return_code() );
+
+  if ( response.status != JobStatus::Success ) {
+    return response;
+  }
+
+  if ( response_proto.executed_thunks_size() != 1 ) {
+    throw runtime_error( "current implementation only supports one thunk execution per response" );
+  }
+
+  for ( const auto & output_proto : response_proto.executed_thunks( 0 ).outputs() ) {
+    response.outputs.push_back( { output_proto.tag(),
+                                  output_proto.hash(),
+                                  output_proto.size(),
+                                  output_proto.executable() } );
+  }
+
+  return response;
+}
+
+ExecutionResponse::ExecutionResponse()
+  : status(), thunk_hash(), outputs()
+{}
---
+++ src/execution/response.hh
@@ -0,0 +1,54 @@
+/* -*-mode:c++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+
+#ifndef EXECUTION_RESPONSE_HH
+#define EXECUTION_RESPONSE_HH
+
+#include <string>
+#include <vector>
+#include <exception>
+#include <stdexcept>
+#include <sys/types.h>
+
+#include "util/optional.hh"
+
+class FetchDependenciesError : public std::exception {};
+class ExecutionError : public std::exception {};
+class UploadOutputError : public std::exception {};
+
+enum class JobStatus
+{
+  Success = 0,
+  RateLimit,
+  InvocationFailure,
+  OperationalFailure,
+  FetchDependenciesFailure,
+  ExecutionFailure,
+  UploadOutputFailure,
+  SocketFailure,
+  ChildProcessFailure,
+};
+
+class ExecutionResponse
+{
+public:
+  struct Output
+  {
+    std::string tag;
+    std::string hash;
+    off_t size;
+    bool is_executable;
+  };
+
+private:
+  ExecutionResponse();
+
+public:
+  JobStatus status;
+
+  std::string thunk_hash;
+  std::vector<Output> outputs;
+
+  static ExecutionResponse parse_message( const std::string & message );
+};
+
+#endif /* REMOTE_RESPONSE_HH */
