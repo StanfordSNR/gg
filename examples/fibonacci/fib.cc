edeb1f768684856c2fe50175de59ed8da9865710
Sadjad Fouladi <sfouladi@gmail.com>
2018-03-20 15:59:10 -0700

    fib.cc: Fix the wrong return code.

--- examples/fibonacci/fib.cc
+++ examples/fibonacci/fib.cc
@@ -7,6 +7,7 @@
 
 #include "thunk/thunk.hh"
 #include "thunk/thunk_writer.hh"
+#include "util/exception.hh"
 #include "util/path.hh"
 #include "util/util.hh"
 
@@ -16,60 +17,66 @@ using namespace gg::thunk;
 
 int main( int argc, char * argv[] )
 {
-  if ( argc != 2 ) {
-    cerr << "usage: fib <N>" << endl;
+  try {
+    if ( argc != 2 ) {
+      cerr << "usage: fib <N>" << endl;
+      return EXIT_FAILURE;
+    }
+
+    const long long N = stoll( argv[ 1 ] );
+
+    if ( N < 0 ) {
+      cerr << argv[ 0 ] << " doesn't accept negative inputs" << endl;
+      return EXIT_FAILURE;
+    }
+
+    if ( N < 2 ) {
+      /* in this case, we just return the value and our job is done */
+      ofstream fout { "out" };
+      fout << ( ( N == 0 ) ? 0 : 1 ) << endl;
+      roost::atomic_create( "", "left" );
+      roost::atomic_create( "", "right" );
+      return EXIT_SUCCESS;
+    }
+
+    string fib_func_hash = safe_getenv( "FIB_FUNCTION_HASH" );
+    string add_func_hash = safe_getenv( "ADD_FUNCTION_HASH" );
+
+    vector<string> envars = { "FIB_FUNCTION_HASH=" + fib_func_hash,
+                              "ADD_FUNCTION_HASH=" + add_func_hash };
+
+    const Thunk fib_left {
+      { fib_func_hash, { "fib", to_string( N - 1 ) }, envars },
+      {},
+      { { fib_func_hash, "" } },
+      { { "out" }, { "left" }, { "right" } }
+    };
+
+    const Thunk fib_right {
+      { fib_func_hash, { "fib", to_string( N - 2 ) }, envars },
+      {},
+      { { fib_func_hash, "" } },
+      { { "out" }, { "left" }, { "right" } }
+    };
+
+    const string fib_left_hash = ThunkWriter::write_thunk( fib_left, "left" );
+    const string fib_right_hash = ThunkWriter::write_thunk( fib_right, "right" );
+
+    const Thunk add_thunk {
+      { add_func_hash, { "add",
+                         thunk::data_placeholder( fib_left_hash ),
+                         thunk::data_placeholder( fib_right_hash ) }, {} },
+      { { fib_left_hash, "" }, { fib_right_hash, "" } },
+      { { add_func_hash, "" } },
+      { { "out" } }
+    };
+
+    ThunkWriter::write_thunk( add_thunk, "out" );
+  }
+  catch ( const exception &  e ) {
+    print_exception( argv[ 0 ], e );
     return EXIT_FAILURE;
   }
 
-  const long long N = stoll( argv[ 1 ] );
-
-  if ( N < 0 ) {
-    cerr << argv[ 0 ] << " doesn't accept negative inputs" << endl;
-    return EXIT_FAILURE;
-  }
-
-  if ( N < 2 ) {
-    /* in this case, we just return the value and our job is done */
-    ofstream fout { "out" };
-    fout << ( ( N == 0 ) ? 0 : 1 ) << endl;
-    roost::atomic_create( "", "left" );
-    roost::atomic_create( "", "right" );
-    return EXIT_SUCCESS;
-  }
-
-  string fib_func_hash = safe_getenv( "FIB_FUNCTION_HASH" );
-  string add_func_hash = safe_getenv( "ADD_FUNCTION_HASH" );
-
-  vector<string> envars = { "FIB_FUNCTION_HASH=" + fib_func_hash,
-                            "ADD_FUNCTION_HASH=" + add_func_hash };
-
-  const Thunk fib_left {
-    { fib_func_hash, { "fib", to_string( N - 1 ) }, envars },
-    {},
-    { { fib_func_hash, "" } },
-    { { "out" }, { "left" }, { "right" } }
-  };
-
-  const Thunk fib_right {
-    { fib_func_hash, { "fib", to_string( N - 2 ) }, envars },
-    {},
-    { { fib_func_hash, "" } },
-    { { "out" }, { "left" }, { "right" } }
-  };
-
-  const string fib_left_hash = ThunkWriter::write_thunk( fib_left, "left" );
-  const string fib_right_hash = ThunkWriter::write_thunk( fib_right, "right" );
-
-  const Thunk add_thunk {
-    { add_func_hash, { "add",
-                       thunk::data_placeholder( fib_left_hash ),
-                       thunk::data_placeholder( fib_right_hash ) }, {} },
-    { { fib_left_hash, "" }, { fib_right_hash, "" } },
-    { { add_func_hash, "" } },
-    { { "out" } }
-  };
-
-  ThunkWriter::write_thunk( add_thunk, "out" );
-
-  return EXIT_FAILURE;
+  return EXIT_SUCCESS;
 }
