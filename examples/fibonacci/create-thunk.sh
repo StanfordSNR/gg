5e8288ba98aca05de740b83f590d593424fe6969
Sadjad Fouladi <sfouladi@gmail.com>
2018-03-20 15:31:31 -0700

    fibonacci: Create a script to generate the initial thunk.

---
+++ examples/fibonacci/create-thunk.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+FIB_HASH=$(gg-hash fib)
+ADD_HASH=$(gg-hash add)
+
+N=${1?"Usage: $0 <N>"}
+
+gg-create-thunk --envar FIB_FUNCTION_HASH=${FIB_HASH} \
+                --envar ADD_FUNCTION_HASH=${ADD_HASH} \
+                --executable ${FIB_HASH} \
+                --output out \
+                --output left \
+                --output right \
+                --placeholder fib${N}_output \
+                ${FIB_HASH} fib ${N}
