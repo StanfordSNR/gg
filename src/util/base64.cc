69cf5eae94194ff2a25b025e95b0071785b96fe2
Sadjad Fouladi <sfouladi@gmail.com>
2018-03-23 23:42:27 -0700

    base64.hh: Create base64 decode method.

---
+++ src/util/base64.cc
@@ -0,0 +1,24 @@
+/* -*-mode:c++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+
+#include "base64.hh"
+
+#include <crypto++/sha.h>
+#include <crypto++/hex.h>
+#include <crypto++/base64.h>
+
+using namespace CryptoPP;
+using namespace std;
+
+string base64::decode( const string & input )
+{
+  SHA256 hash_function;
+  string ret;
+
+  /* Each stage of the Crypto++ pipeline will delete the pointer it owns
+     (https://www.cryptopp.com/wiki/Pipelining) */
+
+  StringSource s( input, true,
+                  new Base64Decoder( new StringSink( ret ) ) );
+
+  return ret;
+}
---
+++ src/util/base64.hh
@@ -0,0 +1,13 @@
+/* -*-mode:c++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+
+#ifndef BASE64_HH
+#define BASE64_HH
+
+#include <string>
+
+namespace base64
+{
+  std::string decode( const std::string & input );
+}
+
+#endif /* BASE64_HH */
