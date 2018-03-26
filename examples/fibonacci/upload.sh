dcb829f58729b854db35827d22d372dcea2d1a63
Sadjad Fouladi <sfouladi@gmail.com>
2018-03-26 16:50:20 -0700

    create-thunk.hh: Explicitly set the path to the fib and add execs.

--- examples/fibonacci/upload.sh
+++ examples/fibonacci/upload.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -e
 
-FIB_PATH=$(dirname $0)/fib
-ADD_PATH=$(dirname $0)/add
+USAGE="$0 <FIB-PATH> <ADD-PATH>"
+
+FIB_PATH=${1?$USAGE}
+ADD_PATH=${2?$USAGE}
 
 gg-put $FIB_PATH $ADD_PATH
