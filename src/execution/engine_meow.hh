ac918c2a6b347a084d852bc9d1688691c7022e65
Sadjad Fouladi <sfouladi@gmail.com>
2018-03-28 14:44:26 -0700

    engine_meow.hh: Add constructor prototype.

--- src/execution/engine_meow.hh
+++ src/execution/engine_meow.hh
@@ -4,8 +4,19 @@
 #define ENGINE_MEOW_HH
 
 #include "engine.hh"
+#include "net/address.hh"
+#include "net/aws.hh"
+#include "net/lambda.hh"
 
 class MeowExecutionEngine : public ExecutionEngine
-{};
+{
+public:
+  MeowExecutionEngine( const AWSCredentials & credentials,
+                       const std::string & region,
+                       const Address & listen_addr,
+                       ExecutionLoop & loop,
+                       SuccessCallbackFunc success_callback,
+                       FailureCallbackFunc failure_callback );
+};
 
 #endif /* ENGINE_MU_HH */
