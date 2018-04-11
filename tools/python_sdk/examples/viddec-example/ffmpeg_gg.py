cca25161e399f21ff2ce1641ba93c0df865aba2e
faromero <faromero@stanford.edu>
2018-04-11 06:56:19 +0000

    Updated ffmpeg_gg.py for min and sec seeking

--- tools/python_sdk/examples/viddec-example/ffmpeg_gg.py
+++ tools/python_sdk/examples/viddec-example/ffmpeg_gg.py
@@ -9,7 +9,7 @@ import subprocess as sp
 from timeit import default_timer as now
 from gg_sdk import GG, GGThunk
 
-CMD = "ffmpeg -loglevel panic -i {video} -frames:v {numout} frameout%03d_{ofile}.jpg"
+CMD = "ffmpeg -ss 00:{min}:{sec} -loglevel panic -i {video} -frames:v {numout} frameout%03d_{ofile}.jpg"
 CMD_IMREC = "li-static {myimage} inception_v3_2016_08_28_frozen.pb imagenet_slim_labels.txt {myoutput}"
 SUFFIX_TO_CLEAR = ['jpg', 'out']
 
@@ -60,26 +60,31 @@ def main(args):
     for vidind, myvid in enumerate(all_chunks):
         if myvid not in all_dur:
           continue
-        all_outname = []
-        for j in range(num_out):
-            all_outname.append('frameout%03d_%03d.jpg' % (j + 1, vidind))
+        vid_dur = int(all_dur[myvid])
+        for i, vd in enumerate(np.arange(0, vid_dur, 0.04 + 0.04 * num_out)):
+          next_min = '%02d' % int(vd / 60)
+          next_sec = '%.2f' % (vd % 60)
+          all_outname = []
+          for j in range(num_out):
+              all_outname.append('frameout%03d_%03d.jpg' % (j + 1, i + vidind))
 
-        next_cmd = CMD.format(video=myvid, numout=num_out, ofile='%03d' % vidind)
-        next_cmd_split = next_cmd.split()
-        gen_jpg_thunk = GGThunk(exe=next_cmd_split[0], outname=all_outname,
-                exe_args=next_cmd_split[1:], args_infiles=False)
-        gen_jpg_thunk.add_infile(myvid)
+          next_cmd = CMD.format(video=myvid, min=next_min, sec=next_sec,
+              numout=num_out, ofile='%03d' % (i + vidind))
+          next_cmd_split = next_cmd.split()
+          gen_jpg_thunk = GGThunk(exe=next_cmd_split[0], outname=all_outname,
+                  exe_args=next_cmd_split[1:], args_infiles=False)
+          gen_jpg_thunk.add_infile(myvid)
 
-        for j in range(num_out):
-            pic_out = 'frameout%03d_%03d_lab.out' % (j + 1, vidind)
-            last_cmd = CMD_IMREC.format(myimage=all_outname[j], myoutput=pic_out)
-            last_cmd_split = last_cmd.split()
-            last_thunk = GGThunk(exe=last_cmd_split[0], outname=pic_out,
-                    exe_args=last_cmd_split[1:], args_infiles=False)
-            last_thunk.add_infile(['inception_v3_2016_08_28_frozen.pb',
-                    'imagenet_slim_labels.txt', (gen_jpg_thunk, all_outname[j])])
+          for j in range(num_out):
+              pic_out = 'frameout%03d_%03d_lab.out' % (j + 1, i + vidind)
+              last_cmd = CMD_IMREC.format(myimage=all_outname[j], myoutput=pic_out)
+              last_cmd_split = last_cmd.split()
+              last_thunk = GGThunk(exe=last_cmd_split[0], outname=pic_out,
+                      exe_args=last_cmd_split[1:], args_infiles=False)
+              last_thunk.add_infile(['inception_v3_2016_08_28_frozen.pb',
+                      'imagenet_slim_labels.txt', (gen_jpg_thunk, all_outname[j])])
 
-            all_thunks.append(last_thunk)
+              all_thunks.append(last_thunk)
 
     end = now()
     delta = end - start
