151ce8e9fa6e6cd67b18afa3752e1505b1deccd6
Sadjad Fouladi <sfouladi@gmail.com>
2018-04-20 14:35:50 -0700

    Update ExCamera example to take advantage of multiple outputs.

--- examples/excamera/gen_makefile.py
+++ examples/excamera/gen_makefile.py
@@ -6,9 +6,8 @@ import math
 VPXENC = 'vpxenc --ivf --codec=vp8 --good --cpu-used=0 --end-usage=cq --min-q=0 --max-q=63 --cq-level={quality} --buf-initial-sz=10000 --buf-optimal-sz=20000 --buf-sz=40000 --undershoot-pct=100 --passes=2 --auto-alt-ref=1 --threads=1 --token-parts=0 --tune=ssim --target-bitrate=4294967295 -o {output}.ivf {input}.y4m'
 TERMINATE_CHUNK = "xc-terminate-chunk {input}.ivf {output}.ivf"
 XC_DUMP_0 = 'xc-dump {input}.ivf {output}.state'
-XC_DUMP_1 = 'xc-dump -S {input_state}.state {input}.ivf {output}.state'
-XC_ENC_FIRST_FRAME = 'xc-enc -W -w 0.75 -i y4m -o {output}.ivf -r -I {source_state}.state -p {input_pred}.ivf {input}.y4m'
-XC_ENC_REBASE = 'xc-enc -W -w 0.75 -i y4m -o {output}.ivf -r -I {source_state}.state -p {input_pred}.ivf -S {pred_state}.state {input}.y4m'
+XC_ENC_FIRST_FRAME = 'xc-enc -W -w 0.75 -i y4m -o {output}.ivf -r -I {source_state}.state -p {input_pred}.ivf {extra} {input}.y4m'
+XC_ENC_REBASE = 'xc-enc -W -w 0.75 -i y4m -o {output}.ivf -r -I {source_state}.state -p {input_pred}.ivf -S {pred_state}.state {extra} {input}.y4m'
 
 def bname(i):
     return "{:08d}".format(i)
@@ -58,23 +57,22 @@ def generate_batch(start, end, quality):
 
             print("## {}\n".format(name))
 
+            base_command = ''
+
             if i == (start + 1):
-                print("{i}.ivf: {i}.y4m {i}-0.ivf {j}-0.state".format(i=name, j=prev_name))
-                print(make_command(XC_ENC_FIRST_FRAME.format(input=name, output=name,
-                                                                  source_state="%s-0" % prev_name,
-                                                                  input_pred="%s-0" % name)))
+                print("{i}-1.state {i}.ivf: {i}.y4m {i}-0.ivf {j}-0.state".format(i=name, j=prev_name))
+                base_command = XC_ENC_FIRST_FRAME.format(input=name, output=name,
+                                                         source_state="%s-0" % prev_name,
+                                                         extra="-O %s-1.state" % name,
+                                                         input_pred="%s-0" % name)
             else:
                 print("{i}-1.ivf: {i}.y4m {i}-0.ivf {j}-0.state".format(i=name, j=prev_name))
-                print(make_command(XC_ENC_FIRST_FRAME.format(input=name, output="%s-1" % name,
-                                                                  source_state="%s-0" % prev_name,
-                                                                  input_pred="%s-0" % name)))
+                base_command = XC_ENC_FIRST_FRAME.format(input=name, output="%s-1" % name,
+                                                         source_state="%s-0" % prev_name,
+                                                         extra='',
+                                                         input_pred="%s-0" % name)
 
-            print()
-
-            if i == (start + 1):
-                print("{i}-1.state: {i}.ivf {j}-0.state".format(i=name, j=prev_name))
-                print(make_command(XC_DUMP_1.format(input=name, input_state="%s-0" % prev_name,
-                                                         output="%s-1" % name)))
+            print(make_command(base_command))
 
         print("# stage 3: rebase\n")
 
@@ -84,14 +82,17 @@ def generate_batch(start, end, quality):
 
             print("## {}\n".format(name))
 
-            print("{i}.ivf: {i}.y4m {i}-1.ivf {j}-0.state {j}-1.state".format(i=name, j=prev_name))
-            print(make_command(XC_ENC_REBASE.format(output=name, input=name, source_state="%s-1" % prev_name,
-                                                         input_pred="%s-1" % name, pred_state="%s-0" % prev_name)))
-
             if i != end:
-                print("{i}-1.state: {i}.ivf {j}-1.state".format(i=name, j=prev_name))
-                print(make_command(XC_DUMP_1.format(input=name, input_state="%s-1" % prev_name, output="%s-1" % name)))
-                print()
+                print("{i}-1.state {i}.ivf: {i}.y4m {i}-1.ivf {j}-0.state {j}-1.state".format(i=name, j=prev_name))
+            else:
+                print("{i}.ivf: {i}.y4m {i}-1.ivf {j}-0.state {j}-1.state".format(i=name, j=prev_name))
+
+            base_command = XC_ENC_REBASE.format(output=name, input=name,
+                                                source_state="%s-1" % prev_name,
+                                                extra=("-O %s-1.state" % name) if i != end else '',
+                                                input_pred="%s-1" % name, pred_state="%s-0" % prev_name)
+
+            print(make_command(base_command))
 
 if __name__ == '__main__':
     if len(sys.argv) != 5:
