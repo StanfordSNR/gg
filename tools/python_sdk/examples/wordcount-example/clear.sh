89453985f028ce33f9a4f5c9e6038b8055e9338c
faromero <faromero@stanford.edu>
2018-04-22 02:12:44 +0000

    Updates to wordcount example

--- tools/python_sdk/examples/wordcount-example/clear.sh
+++ tools/python_sdk/examples/wordcount-example/clear.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rm -rf x* origtext *.out .gg 
+rm -rf x* origtext *.out *.zip .gg 
--- tools/python_sdk/examples/wordcount-example/fetch-deps.sh
+++ tools/python_sdk/examples/wordcount-example/fetch-deps.sh
@@ -6,25 +6,21 @@ cp ../../src/gg_sdk.py .
 
 mkdir -p ${textdir}
 
-# Download 10 books from Project Gutenberg
-wget http://www.gutenberg.org/files/1342/1342-0.txt
-wget http://www.gutenberg.org/files/219/219-0.txt
-wget http://www.gutenberg.org/files/84/84-0.txt
-wget http://www.gutenberg.org/ebooks/844.txt.utf-8
-wget http://www.gutenberg.org/files/98/98-0.txt
-wget http://www.gutenberg.org/files/11/11-0.txt
-wget http://www.gutenberg.org/ebooks/345.txt.utf-8
-wget http://www.gutenberg.org/files/2701/2701-0.txt
-wget http://www.gutenberg.org/files/76/76-0.txt
-wget http://www.gutenberg.org/ebooks/6130.txt.utf-8
+# Download books from Project Gutenberg
+s3bucket="https://s3-us-west-2.amazonaws.com/gg-generic/Gutenberg.zip"
 
-mv *.txt *.utf-8 ${textdir}
+wget $s3bucket
+unzip -j Gutenberg.zip
+
+rm .fuse_hidden* Gutenberg.zip .DS_Store ._*
+
+mv *.txt ${textdir}
 
 # Create input file
 cat ${textdir}* >> inp_text.txt
 
-# Split into 10K chunks
-split -C 10k inp_text.txt
+# Split into 100K chunks
+split -C 100k inp_text.txt
 mv inp_text.txt ${textdir}
 
 # Make
