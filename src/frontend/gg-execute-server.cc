77e033c0c726146587da80fbc55eeaf78d69fe26
Sadjad Fouladi <sfouladi@gmail.com>
2018-04-21 17:48:35 -0700

    gg-execute-server.cc: Use --fix-permissions flag when running gg-execute.

--- src/frontend/gg-execute-server.cc
+++ src/frontend/gg-execute-server.cc
@@ -161,6 +161,7 @@ int main( int argc, char * argv[] )
                     "gg-execute-static",
                     "--get-dependencies",
                     "--put-output",
+                    "--fix-permissions",
                     // "--cleanup"
                   };
 
