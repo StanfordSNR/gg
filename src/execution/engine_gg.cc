314bac1b7f6bd26cd92cded87c3635e786361b77
Sadjad Fouladi <sfouladi@gmail.com>
2018-04-21 17:04:04 -0700

    engine_gg.cc: Update the old-dated engine to behave the same as the others.

--- src/execution/engine_gg.cc
+++ src/execution/engine_gg.cc
@@ -10,6 +10,7 @@
 #include "util/optional.hh"
 #include "util/system_runner.hh"
 #include "util/units.hh"
+#include "util/base64.hh"
 
 using namespace std;
 using namespace gg;
@@ -19,7 +20,7 @@ HTTPRequest GGExecutionEngine::generate_request( const Thunk & thunk )
 {
   string payload = Thunk::execution_payload( thunk );
   HTTPRequest request;
-  request.set_first_line( "POST /cgi-bin/gg/execute.cgi HTTP/1.1" );
+  request.set_first_line( "POST / HTTP/1.1" );
   request.add_header( HTTPHeader{ "Content-Length", to_string( payload.size() ) } );
   request.add_header( HTTPHeader{ "Host", "gg-run-server" } );
   request.done_with_headers();
@@ -35,43 +36,59 @@ void GGExecutionEngine::force_thunk( const Thunk & thunk,
 {
   HTTPRequest request = generate_request( thunk );
 
-  exec_loop.make_http_request<TCPConnection>( thunk.hash(), address_, request,
+  exec_loop.make_http_request<TCPConnection>( thunk.hash(),
+    address_, request,
     [this] ( const uint64_t, const string & thunk_hash,
              const HTTPResponse & http_response ) -> bool
     {
       running_jobs_--;
 
       if ( http_response.status_code() != "200" ) {
-        throw runtime_error( "HTTP failure: " + http_response.status_code() );
+        failure_callback_( thunk_hash, JobStatus::InvocationFailure );
+        return false;
       }
 
       ExecutionResponse response = ExecutionResponse::parse_message( http_response.body() );
 
-      if ( response.status != JobStatus::Success ) {
-        throw runtime_error( "execution failed." );
+      /* print the output, if there's any */
+      if ( response.stdout.length() ) {
+        cerr << response.stdout << endl;
       }
 
-      if ( response.thunk_hash != thunk_hash ) {
-        cerr << http_response.str() << endl;
-        throw runtime_error( "expected output for " + thunk_hash + ", got output for " + response.thunk_hash );
-      }
-
-      for ( const auto & output : response.outputs ) {
-        gg::cache::insert( gg::hash::for_output( response.thunk_hash, output.tag ), output.hash );
-
-        if ( output.data.length() ) {
-          roost::atomic_create( output.data, gg::paths::blob_path( output.hash ) );
+      switch ( response.status ) {
+      case JobStatus::Success:
+      {
+        if ( response.thunk_hash != thunk_hash ) {
+          cerr << http_response.str() << endl;
+          throw runtime_error( "expected output for " +
+                               thunk_hash + ", got output for " +
+                               response.thunk_hash );
         }
+
+        for ( const auto & output : response.outputs ) {
+          gg::cache::insert( gg::hash::for_output( response.thunk_hash, output.tag ), output.hash );
+
+          if ( output.data.length() ) {
+            roost::atomic_create( base64::decode( output.data ),
+                                  gg::paths::blob_path( output.hash ) );
+          }
+        }
+
+        gg::cache::insert( response.thunk_hash, response.outputs.at( 0 ).hash );
+
+        vector<ThunkOutput> thunk_outputs;
+        for ( auto & output : response.outputs ) {
+          thunk_outputs.emplace_back( move( output.hash ), move( output.tag ) );
+        }
+
+        success_callback_( response.thunk_hash, move( thunk_outputs ), 0 );
+
+        break;
       }
 
-      gg::cache::insert( response.thunk_hash, response.outputs.at( 0 ).hash );
-
-      vector<ThunkOutput> thunk_outputs;
-      for ( auto & output : response.outputs ) {
-        thunk_outputs.emplace_back( move( output.hash ), move( output.tag ) );
+      default: /* in case of any other failure */
+        failure_callback_( thunk_hash, response.status );
       }
-
-      success_callback_( response.thunk_hash, move( thunk_outputs ), 0 );
 
       return false;
     },
