d13956e4182b649d8cf9c7caa7fd8bebcdb66ff4
Sadjad Fouladi <sfouladi@gmail.com>
2018-08-23 12:39:30 -0700

    gcloud.hh: Create Google Cloud credentials class.

---
+++ src/net/gcloud.cc
@@ -0,0 +1,27 @@
+/* -*-mode:c++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+
+#include "gcloud.hh"
+
+#include <stdexcept>
+
+#include "util/util.hh"
+
+using namespace std;
+
+const static string GCLOUD_ACCESS_KEY_ENV { "GCLOUD_ACCESS_KEY" };
+const static string GCLOUD_SECRET_KEY_ENV { "GCLOUD_SECRET_KEY" };
+
+GCloudStorageCredentials::GCloudStorageCredentials()
+  : GCloudStorageCredentials( safe_getenv( GCLOUD_ACCESS_KEY_ENV ),
+                              safe_getenv( GCLOUD_SECRET_KEY_ENV ) )
+{}
+
+GCloudStorageCredentials::GCloudStorageCredentials( const string & access_key,
+                                                    const string & secret_key )
+  : access_key_( access_key ), secret_key_( secret_key )
+{
+  if ( access_key_.length() == 0 or secret_key_.length() == 0 )
+  {
+    throw runtime_error( "Missing Google Cloud access key and/or secret key" );
+  }
+}
---
+++ src/net/gcloud.hh
@@ -0,0 +1,23 @@
+/* -*-mode:c++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+
+#ifndef GCLOUD_HH
+#define GCLOUD_HH
+
+#include <string>
+
+class GCloudStorageCredentials
+{
+private:
+  std::string access_key_;
+  std::string secret_key_;
+
+public:
+  GCloudStorageCredentials();
+  GCloudStorageCredentials( const std::string & access_key,
+                            const std::string & secret_key );
+
+  const std::string & access_key() const { return access_key_; }
+  const std::string & secret_key() const { return secret_key_; }
+};
+
+#endif /* GCLOUD_HH */
