c0d11d9538c43a2093ffd8729639f89dc5447917
Sadjad Fouladi <sfouladi@gmail.com>
2018-08-23 13:51:04 -0700

    storage: Add Google Storage backend.

---
+++ src/storage/backend_gs.cc
@@ -0,0 +1,24 @@
+/* -*-mode:c++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+
+#include "backend_gs.hh"
+
+using namespace std;
+using namespace storage;
+
+GoogleStorageBackend::GoogleStorageBackend( const GCloudStorageCredentials & credentials,
+                                            const string & bucket )
+  : client_( { credentials.access_key(), credentials.secret_key() },
+             { "", bucket + ".storage.googleapis.com" } ), bucket_( bucket )
+{}
+
+void GoogleStorageBackend::put( const std::vector<PutRequest> & requests,
+                                const PutCallback & success_callback )
+{
+  client_.upload_files( bucket_, requests, success_callback );
+}
+
+void GoogleStorageBackend::get( const std::vector<GetRequest> & requests,
+                                const GetCallback & success_callback )
+{
+  client_.download_files( bucket_, requests, success_callback );
+}
---
+++ src/storage/backend_gs.hh
@@ -0,0 +1,28 @@
+/* -*-mode:c++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+
+#ifndef STORAGE_BACKEND_GS_HH
+#define STORAGE_BACKEND_GS_HH
+
+#include "backend.hh"
+#include "net/gcloud.hh"
+#include "net/s3.hh"
+
+class GoogleStorageBackend : public StorageBackend
+{
+private:
+  S3Client client_;
+  std::string bucket_;
+
+public:
+  GoogleStorageBackend( const GCloudStorageCredentials & credentials,
+                        const std::string & bucket );
+
+  void put( const std::vector<storage::PutRequest> & requests,
+            const PutCallback & success_callback = []( const storage::PutRequest & ){} ) override;
+
+  void get( const std::vector<storage::GetRequest> & requests,
+            const GetCallback & success_callback = []( const storage::GetRequest & ){} ) override;
+
+};
+
+#endif /* STORAGE_BACKEND_GS_HH */
