8f380e7fa1d7c0806d52ce7457048d0158bdaae4
Sadjad Fouladi <sfouladi@gmail.com>
2018-08-23 12:31:39 -0700

    create-gcloud-function.sh: Minor changes.

--- src/remote/create-gcloud-function.sh
+++ src/remote/create-gcloud-function.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 
 FUNCTION_NAME=gg-gcloud-function
 FUNCTION_PATH=gcloud_function
@@ -8,6 +8,9 @@ TIMEOUT=300
 
 cp common/{common,ggpaths}.py ${FUNCTION_PATH}
 
-gcloud beta functions deploy --region=${REGION} --memory ${MEMORY} ${FUNCTION_NAME} --trigger-http --quiet --runtime=python37 --source=${FUNCTION_PATH} --timeout=${TIMEOUT}
+gcloud beta functions deploy --region=${REGION} --memory ${MEMORY} ${FUNCTION_NAME} --trigger-http --quiet --runtime=python37 --source=${FUNCTION_PATH} --timeout=${TIMEOUT} --format=json
 
 rm ${FUNCTION_PATH}/{common,ggpaths}.py
+
+echo
+echo "export GG_GCLOUD_FUNCTION=[https-trigger-url]"
