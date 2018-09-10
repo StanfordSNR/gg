a742510b247f257f3b6c35318d71b2c6aee0512f
Sadjad Fouladi <sfouladi@gmail.com>
2018-09-10 16:37:12 -0700

    gg-put.cc: Read the file list from stdin.

--- src/frontend/gg-put.cc
+++ src/frontend/gg-put.cc
@@ -21,25 +21,59 @@ int main( int argc, char * argv[] )
       abort();
     }
 
-    if ( argc < 2 ) {
-      usage( argv[ 0 ] );
-      return EXIT_FAILURE;
+    bool read_names_from_stdin = false;
+    constexpr size_t BATCH_SIZE = 64 * 32;
+
+    if ( argc == 1 ) {
+      read_names_from_stdin = true;
     }
 
     unique_ptr<StorageBackend> storage_backend = StorageBackend::create_backend( gg::remote::storage_backend_uri() );
 
     vector<storage::PutRequest> put_requests;
-    for ( int i = 1; i < argc; i++ ) {
-      const string filename { argv[ i ] };
-      const string file_hash = gg::hash::file_force( filename );
-      put_requests.emplace_back( filename, file_hash, gg::hash::to_hex( file_hash ) );
+    int arg_index = 1;
+    bool last_batch = false;
+    string line;
+
+    while ( not last_batch ) {
+      string file_name;
+      string file_hash;
+
+      if ( read_names_from_stdin ) {
+        if ( getline( cin, line ) ) {
+          file_name = line;
+        }
+        else {
+          last_batch = true;
+        }
+      }
+      else {
+        if ( arg_index < argc ) {
+          file_name = argv[ arg_index++ ];
+        }
+        else {
+          last_batch = true;
+        }
+      }
+
+      if ( not last_batch ) {
+        file_hash = gg::hash::file_force( file_name );
+        if ( not storage_backend->is_available( file_hash ) ) {
+          put_requests.emplace_back( file_name, file_hash, gg::hash::to_hex( file_hash ) );
+        }
+      }
+
+      if ( put_requests.size() >= BATCH_SIZE or
+           ( put_requests.size() > 0 and last_batch ) ) {
+        storage_backend->put( put_requests,
+          [&storage_backend]( const storage::PutRequest & request ) {
+            storage_backend->set_available( request.object_key );
+            cerr << "PUT " << request.filename.string() << " -> " << request.object_key << endl;
+          } );
+
+        put_requests.clear();
+      }
     }
-    storage_backend->put( put_requests,
-      []( const storage::PutRequest & request )
-      {
-        cerr << "PUT " << request.filename.string() << " -> "
-             << request.object_key << endl;
-      } );
   }
   catch ( const exception &  e ) {
     print_exception( argv[ 0 ], e );
