bd133d74cb5fe3c56388bb45dcc187a767e9b898
Sadjad Fouladi <sfouladi@gmail.com>
2018-09-09 23:03:42 -0700

    Create splice-lines.cc

---
+++ src/frontend/splice-lines.cc
@@ -0,0 +1,24 @@
+/* -*-mode:c++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+
+#include <iostream>
+#include <string>
+#include <cctype>
+
+using namespace std;
+
+int main()
+{
+  string line;
+
+  while ( getline( cin, line ) ) {
+    if ( line.back() == '\\' ) {
+      line.pop_back();
+      cout << line;
+    }
+    else {
+      cout << line << endl;
+    }
+  }
+
+  return 0;
+}
