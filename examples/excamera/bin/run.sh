14cf295b84d3291410e2ac676a2592b9744bed9d
Nikolay Sverchkov <ssnikolay@gmail.com>
2019-08-29 20:27:47 +0300

    Rename dev-env to sandbox-env; Add comments to Dockerfile

--- examples/excamera/bin/run.sh
+++ examples/excamera/bin/run.sh
@@ -6,9 +6,22 @@ JOBS_COUNT=${1?$USAGE}
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR/../
 
+printf "1. Generate Makefile\n"
 ./gen_makefile.py 0 6 16 1 > Makefile
+
+printf "2. Initialize gg\n"
+rm -rf .gg
 gg init
+
+printf "3. Execute 'make' to create thunks\n"
 gg-infer make -j$(nproc)
+
+printf "4. Run video processing jobs\n"
 gg force --engine=lambda --jobs=$JOBS_COUNT *.ivf
+
+printf "5. Build output.avi\n"
 ls *-vpxenc.ivf | while read each; do echo "file '$each'" >> mylist.txt; done
 ffmpeg -f concat -i mylist.txt -codec copy output.avi
+
+printf "6. Result:\n"
+file output.avi
