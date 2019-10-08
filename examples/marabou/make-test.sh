a579e45765f26988a2cf022e1e4a8c09129e0a4d
Sadjad Fouladi <sfouladi@gmail.com>
2019-10-08 14:39:49 -0700

    examples/marabou: Create a script to generate an example.

---
+++ examples/marabou/make-test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -e
+
+USAGE="$0 <SLEEP-PATH> <MERGE-PATH>"
+
+SLEEP_PATH=${1?$USAGE}
+MERGE_PATH=${2?$USAGE}
+
+SLEEP_HASH=$(gg-hash $SLEEP_PATH)
+MERGE_HASH=$(gg-hash $MERGE_PATH)
+
+gg init
+gg collect $SLEEP_PATH
+gg collect $MERGE_PATH
+
+HASHES=$(for i in 1 10 30 60
+do
+  gg create-thunk --executable $(gg hash $SLEEP_PATH)=sleep --output output \
+                  $(gg hash $SLEEP_PATH) sleep ${i} "SLEEP(${i})" 2>&1
+done)
+
+gg create-thunk`for h in ${HASHES}; do echo -n ' --thunk' ${h}; done` \
+                --executable $(gg hash $MERGE_PATH)=merge \
+                --placeholder out --output output \
+                $(gg hash $MERGE_PATH) merge \
+                `for h in ${HASHES}; do echo -n ' '@{GGHASH:${h}}; done`
